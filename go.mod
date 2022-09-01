66449300596a9db7bc46a68f4e6d73b23b1bb132
zhanghaisheng <zhanghaisheng@qimao.com>
2022-09-01 19:27:44 +0800

    test函数

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hisheng/xormmodel
+
+go 1.17
---
+++ xormmodel.go
@@ -0,0 +1,10 @@
+/**
+@author 张海生<zhanghaisheng@qimao.com>
+@dateTime   : 2022/9/1 7:26 下午
+@Desc
+*/
+package xormmodel
+
+func Test() string {
+	return "test xorm model"
+}
