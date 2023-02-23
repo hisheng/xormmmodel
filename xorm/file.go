119f7ad44c7f484f656f3d77862a45e816f0e500
zhanghaisheng <465908774@qq.com>
2023-02-23 11:00:21 +0800

    zuixin

--- xorm/file.go
+++ xorm/file.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-//把字符串写入到文件中 类似于php file_put_contents
+// 把字符串写入到文件中 类似于php file_put_contents
 func filePutContents(filename string, data string) error {
 	f, err := os.Create(filename)
 	defer f.Close()
@@ -21,7 +21,7 @@ func filePutContents(filename string, data string) error {
 	return nil
 }
 
-//判断文件/文件夹是否存在 -功能类似php的file_exists
+// 判断文件/文件夹是否存在 -功能类似php的file_exists
 func FileExists(path string) bool {
 	_, err := os.Stat(path)
 	if err != nil {
@@ -45,9 +45,9 @@ func saveToFile(tableName string, structStrArr []string) {
 	structStr += "} \n"
 	fileStr += structStr
 
-	//2 TableName
+	// 2 TableName
 	tbStr := fmt.Sprintf("func (m *%s) TableName() string { \n return \"%s\" \n }\n", upperCamelCase(tableName), tableName)
-	//fmt.Println(fileStr)
+	// fmt.Println(fileStr)
 	fileStr += tbStr
 	saveFile(tableName, fileStr)
 
@@ -66,7 +66,7 @@ func saveFile(tableName, fileStr string) error {
 	return nil
 }
 
-//大驼峰式命名法（upper camel case）
+// 大驼峰式命名法（upper camel case）
 func upperCamelCase(s string) string {
 	strArr := strings.Split(s, "_")
 	upperCamelSting := ""
@@ -86,12 +86,12 @@ type YamlFile struct {
 }
 
 func ReadYamlFile(yamlFilePath string) YamlFile {
-	//1读取文件
+	// 1读取文件
 	data, err := ioutil.ReadFile(yamlFilePath)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//2解析文件
+	// 2解析文件
 	var y YamlFile
 	err = yaml.Unmarshal(data, &y)
 	fmt.Println(y, err)
@@ -112,6 +112,16 @@ func ConfigFilePath() string {
 			break
 		}
 		if p == "api-ad.qmniu.com" {
+			arrivalProjPath = true
+			break
+		}
+
+		if p == "pixiu-ads-server" {
+			arrivalProjPath = true
+			break
+		}
+
+		if p == "pixiu-ad-backend" {
 			arrivalProjPath = true
 			break
 		}
